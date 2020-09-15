f9ebc02e8c772f55982cb122a507e5dbdaf90cdd
Julie Larkin <julielarkin@Julies-MacBook-Air.local>
2020-09-14 19:05:34 -0700

    Studio 5

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.junit.Test;
+import main.BalancedBrackets;
 
 import static org.junit.Assert.*;
 
@@ -12,5 +13,75 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void testNoBrackets(){
+        String test = "";
+        boolean retVal = false;
 
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertTrue(retVal);
+    }
+
+    @Test
+    public void testOpenBracket(){
+        String test = "[";
+        boolean retVal = true;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertFalse(retVal);
+    }
+
+    @Test
+    public void onlyBrackets(){
+        String test = "[]";
+        boolean retVal = false;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertTrue(retVal);
+    }
+
+    @Test
+    public void testClosedBracket(){
+        String test = "]";
+        boolean retVal = true;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertFalse(retVal);
+    }
+
+    @Test
+    public void testBracketsOrder(){
+        String test = "][";
+        boolean retVal = true;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertFalse(retVal);
+    }
+
+    @Test
+    public void testCharsAndBrackets(){
+        String test = "ab[c12]3";
+        boolean retVal = false;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertTrue(retVal);
+    }
+
+    @Test
+    public void testCharsAndReversedBrackets(){
+        String test = "aBc]ij[";
+        boolean retVal = true;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertFalse(retVal);
+    }
+
+    @Test
+    public void testCharsAndOneBracket(){
+        String test = "v4sj[sk";
+        boolean retVal = true;
+
+        retVal = BalancedBrackets.hasBalancedBrackets(test);
+        assertFalse(retVal);
+    }
 }
